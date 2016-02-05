0042c56a706ed8eeba6f2f87e192996e6e3c967f
stuart nelson <stuartnelson3@gmail.com>
2016-02-05 23:29:04 +0100

    exit on bad cl flags

--- dd_copy.c
+++ dd_copy.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
         if (argc%2 != 1) {
                 printf("USAGE: %s -i <input> -o <output>\n", argv[0]);
+                exit(1);
         }
         int i;
         int BUFFER_SIZE = 512;
